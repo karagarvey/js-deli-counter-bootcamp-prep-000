e240b99816f95597d8e3d87e65b8bfee09abeb23
karagarvey <kgarvey87@gmail.com>
2018-03-21 16:43:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function currentLine(katzDeliLine) {
     for (var i = 0; i > katzDeliLine.length; i++) {
       var linePhrase =  []
       linePhrase.push('${i + 1} + ". " + ${katzDeliLine[i]}')
+      console.log(linePhrase)
     }
   return `The line is currently: ${linePhrase}`
   }
