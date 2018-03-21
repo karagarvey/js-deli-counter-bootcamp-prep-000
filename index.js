ae55aa913e55e25b7cac99ed9757d934aa96a8a7
karagarvey <kgarvey87@gmail.com>
2018-03-21 16:27:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    console.log(katzDeliLine)
+    return `Currently serving ${katzDeliLine[0]}.`
+    katzDeliLine.splice[0,1]
+    console.log(katzDeliLine)
+  } else {
+      return 'There is nobody waiting to be served!'
+    }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length > 0){
+      return 'The line is currently empty.'
+  } else {
+    for (var i = 0; i > katzDeliLine.length; i++) {
+      return `The line is currently: ${i + 1}. ${katzDeliLine[i]}, `;
+    }
+  }
+}
