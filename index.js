23d49d0885e72aed6459b6aaceb16fbcb3128cac
karagarvey <kgarvey87@gmail.com>
2018-03-21 19:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function currentLine(katzDeliLine) {
       console.log("i:" + i)
       console.log("name at i: " + katzDeliLine[i])
     }
-  } return `The line is currently: ${linePhrase.slice(-2)}`
+  } return `The line is currently: ${linePhrase}`
 }
